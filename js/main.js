0e3f92eaaab019c2caf314f2931719d1419ffa28
Matheus Cassioli <matheuscassioli@hotmail.com>
2021-07-14 20:47:21 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var nome = "Matheus Cassioli";
+var idade = 23;
+var idade2 = 2;
+var frase = "Japão é o melhor time do Mundo";
+
+var n1 = 10;
+var n2 = 200;
+// alert(nome + " tem " + idade + " anos ");
+// alert (idade + idade2);
+
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão","Itália")); //Substitui Japão por Itália
+console.log(frase.toLowerCase()); //Passa as letras para minúscula
+console.log(frase.toUpperCase()); //Passa as letras para maiúscula
+
+console.log(n1 * idade);
