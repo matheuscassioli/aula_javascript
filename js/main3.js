55559e92786cd9320494faaa09464505b0585c58
Matheus Cassioli <matheuscassioli@hotmail.com>
2021-07-14 21:09:23 -0300

    localizando com array

--- js/main3.js
+++ js/main3.js
@@ -1,3 +1,10 @@
-/*var fruta = {nome: "macã", cor: "vermelha"};
+/* var fruta = {nome: "macã", cor: "vermelha"};
 console.log(fruta.nome);
-console.log(fruta.cor); */
+console.log(fruta.cor);
+console.log(fruta.nome.log); */
+
+var frutas = [{nome: "macã", cor: "vermelha"} , {nome:"uva", cor: "roxa"}];
+
+console.log(frutas[0].nome);
+console.log(frutas[0].cor);
+
