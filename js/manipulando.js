1e804cf93a470c12c796755c5224f18e44c1a992
Matheus Cassioli <matheuscassioli@hotmail.com>
2021-07-19 20:01:24 -0300

    manipulando o DOM - window.open/window.location.href

--- js/manipulando.js
+++ js/manipulando.js
@@ -2,6 +2,6 @@ function botao(){
     document.getElementById("clicou").innerHTML = "Obrigado pro clicar";     // resgata o id e substitui a frase mediante a uma ação
 }
 function redirecionar(){
-    window.open("https://www.google.com/?client=safari")    //  ao clicar redireciona para uma nova aba no google
-    window.location.href = "https://www.google.com/?client=safari"  // ao clicar redireciona para duas novas abas no google sobrepondo sua página   
+//window.open("https://www.google.com/?client=safari");  // ABRE UMA NOVA ABA NO SITE DESEJADO
+window.location.href = "https://www.google.com/?client=safari"  // SOBREPOE SUA ABA DE TRABALHO
 }
